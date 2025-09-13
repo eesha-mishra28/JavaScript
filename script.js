24ebbe2b774c2312037fa7094c4d07050c3bbf56
Eesha Mishra V <eeshamishra92@gmail.com>
2025-09-13 22:04:43 +0530

    Update script.js with new changes

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ if(num%2!=0){
   if(i%2==0){
   console.log(i);}
 }*/
+/*let gamenum=25;
+let usernum=prompt("Guess a number");
+while(usernum!=gamenum){
+  usernum= prompt("Guess again");
+} 
+console.log("you are right");*/
+/*let marks= [99,98,98,97,90];
+let sum=0;
+for(let i=0;i<marks.length;i++){
+  sum=sum+marks[i];
+}
+let avg=sum/marks.length;
+console.log(avg);*/
+let prize=[99,90,98];
+for(let i=0;i<prize.length;i++){
+  prize[i]=prize[i]/10;
+}
+console.log(prize);
