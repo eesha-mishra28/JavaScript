0f6ffc35408636ade2505bd8bfd71aff402b64f8
Eesha Mishra V <eeshamishra92@gmail.com>
2025-09-17 22:31:28 +0530

    Update script.js with latest changes

--- script.js
+++ script.js
@@ -31,6 +31,19 @@ for(let i=0;i<prize.length;i++){
   prize[i]=prize[i]/10;
 }
 console.log(prize);*/
-let companies=["a","b","c"];
+/*let companies=["a","b","c"];
 console.log(companies.shift());
-companies.splice(2,1,"d");
+companies.splice(2,1,"d");*/
+
+/*function fun(str){
+  let count=0;
+  for(let i=0; i<str.length;i++){
+    if(str[i]=="a" || str[i]=="e"|| str[i]=="i"||str[i]=="o"||str[i]=="u"){
+      count++;
+    }
+  }
+  console.log(count);
+  return count;
+}*/
+let num=[1,2,3];
+num.forEach(val => console.log(val*val));
